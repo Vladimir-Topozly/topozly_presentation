1cdaa5037428ca8e3823aec7b5dffaa000f87f54
Vladimir Topozly <vovatoposli@gmail.com>
2019-02-19 22:54:01 +0200

    fd

--- sample-web-selenium-project/src/test/java/MyFirstTest.java
+++ sample-web-selenium-project/src/test/java/MyFirstTest.java
@@ -1,10 +1,12 @@
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class MyFirstTest {
     ChromeDriver wd;
+    FirefoxDriver fd;
 
     @BeforeMethod
     public void setUp() {
@@ -22,5 +24,5 @@ public class MyFirstTest {
         Thread.sleep(3000);
         wd.quit();
     }
-    
+
 }
